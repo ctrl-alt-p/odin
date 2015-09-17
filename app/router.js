121774f13c07387319a600841e9155c0702d8c00
Glenn Nagel <glenn@mercury-wireless.com>
2015-09-17 17:34:26 -0400

    Added more market data and watchlists

--- app/router.js
+++ app/router.js
@@ -2,49 +2,50 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+    location: config.locationType
 });
 
 Router.map(function() {
-  this.route('user', function() {
-    this.route('profile');
-    this.route('balances');
-    this.route('positions');
-    this.route('history');
-    this.route('gainloss');
-    this.route('orders');
-  });
-
-  this.route('accounts', function() {
-    this.route('balances');
-    this.route('positions');
-    this.route('history');
-    this.route('gainloss');
-    this.route('orders');
-  });
-
-  this.route('trading', function() {
-    this.route('orders');
-  });
-
-  this.route('market', function() {
-    this.route('quotes');
-    this.route('timeandsales');
-    this.route('option-chains');
-    this.route('option-strikes');
-    this.route('option-expirations');
-    this.route('history');
-    this.route('clock');
-    this.route('calendar');
-    this.route('search');
-    this.route('lookup');
-    this.route('events', function() {
-      this.route('session');
+    this.route('user', function() {
+        this.route('profile');
+        this.route('balances');
+        this.route('positions');
+        this.route('history');
+        this.route('gainloss');
+        this.route('orders');
     });
 
-    this.route('fundamentals', function() {});
-  });
-  this.route('watchlists');
+    this.route('accounts', function() {
+        this.route('balances');
+        this.route('positions');
+        this.route('history');
+        this.route('gainloss');
+        this.route('orders');
+    });
+
+    this.route('trading', function() {
+        this.route('orders');
+    });
+
+    this.route('market', function() {
+        this.route('quotes');
+        this.route('timeandsales');
+        this.route('option-chains');
+        this.route('option-strikes');
+        this.route('option-expirations');
+        this.route('history');
+        this.route('clock');
+        this.route('calendar');
+        this.route('search');
+        this.route('lookup');
+        this.route('events', function() {
+            this.route('session');
+        });
+
+        this.route('fundamentals', function() {});
+    });
+    this.route('watchlists');
 });
 
-export default Router;
+export
+default Router;
