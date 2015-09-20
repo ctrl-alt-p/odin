9d151f2c54c21e456b89f3f6d0c8429b5dd3ad71
Glenn Nagel <glenn@mercury-wireless.com>
2015-09-20 15:30:21 -0400

    Stripping the app back down to bare-bones install

--- ember-app/app/app.js
+++ ember-app/app/app.js
@@ -3,31 +3,13 @@ import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
-var App;
-
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-App = Ember.Application.extend({
+var App = Ember.Application.extend({
     modulePrefix: config.modulePrefix,
     podModulePrefix: config.podModulePrefix,
     Resolver: Resolver,
-    // >> debugger //
-    LOG_TRANSITIONS_INTERNAL: true,
-    // LOG_ACTIVE_GENERATION: true,
-    // LOG_VIEW_LOOKUPS: true,
-    // LOG_RESOLVER: true
-    // << debugger //
 });
-
-// >> debugger //
-// Ember.run.backburner.DEBUG = true;
-// Ember.ENV.RAISE_ON_DEPRECATION = true;
-// Ember.LOG_STACKTRACE_ON_DEPRECATION = true;
-Ember.LOG_BINDINGS = true;
-// Ember.RSVP.on('error', function(error) {
-//     Ember.Logger.assert(false, error);
-// });
-// << debugger //
 
 
 loadInitializers(App, config.modulePrefix);
